0406bb788081e7b398c00893f2db99126e28861a
Ubuntu <ubuntu@ip-172-31-11-242.ap-northeast-1.compute.internal>
2023-02-16 09:53:39 +0000

    changed code inside jenkin2.java file

--- jenkin2.java
+++ jenkin2.java
@@ -1,5 +1,5 @@
 class jenkin2{
       public static void main(String[] args) {
-                 System.out.println("hello, world!!!");
+                 System.out.println("helloooooooooooo");
 }
 }
